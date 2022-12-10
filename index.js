c6a16c2cbe3231b3d58c0927ca6b96b1a3f2d5eb
David Aviles <aviles.david1@gmail.com>
2022-12-10 17:29:24 -0500

    express api with basic auth

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const basicAuth = require('express-basic-auth')
+
+// App setup
+const app = express()
+
+// App port
+const PORT = 80
+
+// Use body parse for JSON
+app.use(bodyParser.json())
+
+app.use(basicAuth({
+  authorizer: (username, password) => {
+    const userMatches = basicAuth.safeCompare(username, 'admin')
+    const passwordMatches = basicAuth.safeCompare(password, 'secret')
+    return userMatches & passwordMatches
+  }, 
+  unauthorizedResponse: (req) => {
+    return `Unauthorized. You must authenticate. Your ip: ${req.ip}`
+  }
+}))
+
+
+// Route
+app.get('/api/auth', (req, res) => {
+  res.send('authorized');
+});
+
+// Start express on port 3000
+app.listen(PORT, () => {
+  console.log(`Server Started on ${PORT}`)
+})
